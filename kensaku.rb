ed641d26002edf0ef1eaa74e21ca60d06b434b80
John Mettraux <jmettraux@gmail.com>
2013-08-26 23:12:13 +0900

    reset style.scss

--- kensaku.rb
+++ kensaku.rb
@@ -7,16 +7,16 @@ configure do
 
   Compass.configuration do |c|
     c.project_path = File.dirname(__FILE__)
-    c.sass_dir = 'views'
   end
 
   set :haml, :format => :html5
-  set :sass, Compass.sass_engine_options
   set :scss, Compass.sass_engine_options
 end
 
-#get '/sass.css' do; sass :sass_file; end
-get '/scss.css' do; sass :scss_file; end
+get '/style.css' do
+
+  scss :style
+end
 
 get '/' do
 
