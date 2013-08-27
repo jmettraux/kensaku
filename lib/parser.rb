6e7efa734b7080da74e76886cbc04464b27b3329
John Mettraux <jmettraux@gmail.com>
2013-08-28 06:39:24 +0900

    split romaji

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,43 @@
+# encoding: utf-8
 
 require 'mojinizer'
 
+
 R = /^([^;\s]+)(?:;([^:\s]+))* (?:\[([^;\s]+)(?:;([^:\s]+))*\] )?\/(.+)\/$/
 ENTL = /^EntL\d+X?$/
+
+PREX = %w[ ki shi chi ni hi mi ri gi ji bi pi ]
+
+def merge_romaji(syl0, syl1)
+
+  if syl0 == 'ji' || syl0 == 'chi' || syl0 == 'shi'
+    syl0.chars.first + syl1.chars.to_a.last
+  else
+    syl0.chars.first + syl1.chars.to_a[1..-1].join
+  end
+end
+
+def compact_romaji(syllables)
+
+  a = []
+  while syllables.length > 0
+    syl = syllables.shift
+    nsyl = syllables.first
+    if syl == 'h!'
+      a << nsyl.chars.first
+    elsif nsyl && PREX.index(syl)
+      if nsyl.chars.first == 'x'
+        syllables.shift
+        a << merge_romaji(syl, nsyl)
+      else
+        a << syl
+      end
+    else
+      a << syl
+    end
+  end
+  a
+end
 
 def parse_line(l)
 
@@ -12,18 +47,25 @@ def parse_line(l)
 
   kana = [ m[3], *(m[4] ? m[4].split(';') : []) ].compact
 
-  romaji = kana.collect(&:romaji)
-  romaji = kanji.select(&:kana?).collect(&:romaji) if romaji.empty?
-  romaji = romaji.collect { |s| s.split('(').first }.uniq
+  syllabs = kana.empty? ? kanji : kana
+  syllabs = syllabs.select(&:kana?)
+  syllabs = syllabs.collect { |s| s.split('(').first }.uniq
+
+  romaji = syllabs.collect(&:romaji)
+
+  split_romaji = syllabs.collect { |k| k.chars.collect(&:romaji) }
+  split_romaji = split_romaji.collect { |s| compact_romaji(s) }
 
   glosses = m[5].split('/').reject { |g| ENTL.match(g) }
 
   p kanji
   p kana
   p romaji
+  p split_romaji
   p glosses
   puts
 end
+
 
 count = 0
 while true
@@ -31,6 +73,12 @@ while true
   break unless line
   count += 1
   parse_line(line)
-  break if count == 100
+  break if count == 200
 end
+
+#%w[ xya xyo xyu ].each { |k| puts k.hiragana + " " + k.katakana }
+#%w[ ya yo yu ].each { |k| puts k.hiragana + " " + k.katakana }
+#%w[ tsu h! ].each { |k| puts k.hiragana + " " + k.katakana }
+#p "かっこい".chars.collect(&:romaji)
+#p compact_romaji("かっこい".chars.collect(&:romaji))
 
